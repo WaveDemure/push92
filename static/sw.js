43f0603dd3ab446f18a1c615c58796d88d1966cc
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2023-11-11 19:24:19 +0000

    bye bye al of it

--- static/sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("/h/mathematics.js");
-importScripts("/h/geography.js");
-importScripts("/h/english.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
